09ca5a79b778d804b87c49f4fa3ccbdcffd7a074
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-12-04 17:15:06 -0600

    Made sure ellipsis show on overflow

--- src/utils/styledComponents.js
+++ src/utils/styledComponents.js
@@ -3,6 +3,9 @@ import { Typography, Divider } from "@mui/joy";
 
 export const StyledTypography = styled(Typography)(({ theme }) => ({
     textAlign: "center !important",
+    overflowX: 'hidden', 
+    textOverflow: 'ellipsis', 
+    whiteSpace: 'nowrap', 
 }));
 
 export const StyledTypographyClassification = styled(Typography)(({ theme }) => ({
@@ -12,11 +15,11 @@ export const StyledTypographyClassification = styled(Typography)(({ theme }) => 
 
 export const StyledTypographyWord = styled(Typography)(({ theme }) => ({
     overflowX: 'hidden', 
-        textOverflow: 'ellipsis', 
-        whiteSpace: 'nowrap', 
-        fontWeight: 500, 
-        textAlign: 'center',
-        position: 'relative',
+    textOverflow: 'ellipsis', 
+    whiteSpace: 'nowrap', 
+    fontWeight: 500, 
+    textAlign: 'center',
+    position: 'relative',
 }));
 
 
