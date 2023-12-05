0e65a378d612261dde1af73395e6a82d925b946e
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-12-05 00:22:43 -0600

    Bigger flashcard on laptop

--- src/components/Words/word-modal/word-modal.component.jsx
+++ src/components/Words/word-modal/word-modal.component.jsx
@@ -27,7 +27,7 @@ const WordModal = forwardRef(({ word, checks }, ref) => {
           marginTop: 2,
           borderRadius: 10,
           py:2,
-          width: isMobile ? '95%' : isTablet ? '80%' : isLaptop ? '60%' : '40%',
+          width: isMobile ? '95%' : isTablet ? '80%' : isLaptop ? '75%' : '40%',
         }}>
 
       {word ? (
